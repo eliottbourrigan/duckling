1f125050656dd8583870b0b92fa51690f6761592
Eliott BOURRIGAN <eliott.bourrigan@iodagroup.com>
2024-06-04 15:12:38 +0200

    Add authentication

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from fastapi.middleware.cors import CORSMiddleware
 from duckduckgo_search import DDGS
 
 app = FastAPI()
+security = HTTPBasic()
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,8 +14,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = "eliott"
+    correct_password = "dcA9X77y2DG8pw"
+    if not (credentials.username == correct_username and credentials.password == correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
 @app.get("/search")
-def search(keywords: str, region: str = "wt-wt", safesearch: str = "moderate", timelimit: str = None, backend: str = "api", max_results: int = None):
+def search(
+    keywords: str, 
+    region: str = "wt-wt", 
+    safesearch: str = "moderate", 
+    timelimit: str = None,
+    backend: str = "api",
+    max_results: int = None,
+    username: str = Depends(authenticate)  # Include the authenticate dependency here
+):
     try:
         result = DDGS().text(keywords, region, safesearch, timelimit, backend, max_results)
     except Exception as e:
